e87970a069d3c30e1147f62148bafeecdb02b61e
Australopithecus Man Person <ZHENGSTA7@gmail.com>
2017-01-17 10:49:33 -0500

    Made changes to AutoDriveDistance

--- src/org/usfirst/frc/team4946/robot/commands/AutoDriveDistance.java
+++ src/org/usfirst/frc/team4946/robot/commands/AutoDriveDistance.java
@@ -32,7 +32,7 @@ public class AutoDriveDistance extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (distance <= m_distanceToDrive);
+        return (distance >= m_distanceToDrive);
     }
 
     // Called once after isFinished returns true
