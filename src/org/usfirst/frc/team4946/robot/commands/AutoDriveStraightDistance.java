2004bf78e4fe38855bb30d3cfbd2eef6b53eccf1
Australopithecus Man Person <ZHENGSTA7@gmail.com>
2017-01-17 11:19:25 -0500

    Another unsaved change apparently that just randomly appeared

--- src/org/usfirst/frc/team4946/robot/commands/AutoDriveStraightDistance.java
+++ src/org/usfirst/frc/team4946/robot/commands/AutoDriveStraightDistance.java
@@ -42,10 +42,12 @@ public class AutoDriveStraightDistance extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveSubsystem.drive(0.0, 0.0, 0.0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.driveSubsystem.drive(0.0, 0.0, 0.0);
     }
 }
