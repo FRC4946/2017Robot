bfa627c9b79187699ada09035728f0ccee471ff7
Australopithecus Man Person <ZHENGSTA7@gmail.com>
2017-01-17 11:14:40 -0500

    Updated the AutoDriveDistance command to include the Gyroscope and the additional maxSpeed argument. Not sure if the gyroscope is used correctly though? Someone pls check. Also AutoDriveDistance is now named AutoDriveStraightDistance.

--- src/org/usfirst/frc/team4946/robot/commands/AutoDriveStraightDistance.java
+++ src/org/usfirst/frc/team4946/robot/commands/AutoDriveStraightDistance.java
@@ -7,32 +7,37 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class AutoDriveDistance extends Command {
+public class AutoDriveStraightDistance extends Command {
 
 	double m_distanceToDrive;
-	double distance;
+	double m_startingDistance;
+	double m_distanceTraveled;
+	double m_gyroAngle;
+	double m_maxSpeed;
 	
-    public AutoDriveDistance(double distanceToDrive) {
+    public AutoDriveStraightDistance(double distInches, double maxSpeed) {
     	requires(Robot.driveSubsystem);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	m_distanceToDrive = distanceToDrive;
+    	m_distanceToDrive = distInches;
+    	m_maxSpeed = maxSpeed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	m_startingDistance = Robot.driveSubsystem.getEncoderDistance();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveSubsystem.drive(0.5, 0.0, 0.0);
-    	distance = Robot.driveSubsystem.getEncoderDistance();
+       	m_gyroAngle = Robot.driveSubsystem.getGyroValues();
+    	Robot.driveSubsystem.drive(m_maxSpeed/2, m_gyroAngle, 0.0);
+    	m_distanceTraveled = Robot.driveSubsystem.getEncoderDistance() - m_startingDistance;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (distance >= m_distanceToDrive);
+        return (m_distanceTraveled >= m_distanceToDrive);
     }
 
     // Called once after isFinished returns true
