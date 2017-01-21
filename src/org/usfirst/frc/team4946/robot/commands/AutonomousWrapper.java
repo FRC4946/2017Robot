a6d03f6a09aeca73558e11a6169c078bfc515a8f
Australopithecus Man Person <ZHENGSTA7@gmail.com>
2017-01-21 12:00:45 -0500

    Have Autonomous codes until shooting. Began to include SmartDashboard features. Still need more measurements, as well as the Vision and Shooter subsystem.

--- src/org/usfirst/frc/team4946/robot/commands/AutonomousWrapper.java
+++ src/org/usfirst/frc/team4946/robot/commands/AutonomousWrapper.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4946.robot.commands;
 
 import org.usfirst.frc.team4946.robot.RobotConstants;
 
+import DriveTrainCommands.AutoDriveDistance;
 import DriveTrainCommands.AutoDriveDistancePID;
 import DriveTrainCommands.TurnPID;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -33,10 +34,14 @@ public class AutonomousWrapper extends CommandGroup {
     	
     	switch (position){
     		case RobotConstants.LEFT_POSITION_SHOOT: 
+    			addSequential(new AutoDriveDistancePID (67.662,1.0));
+    			addSequential(new TurnPID (60.0, 0.5));
+    			
     			break;
     		case RobotConstants.LEFT_POSITION_NO_SHOOT: 
     			addSequential(new AutoDriveDistancePID(67.662, 1.0));
     			addSequential(new TurnPID(60.0, 0.5));
+    			
     			break;
     		case RobotConstants.RIGHT_POSITION_NO_SHOOT:
     			//Drives 
@@ -45,10 +50,27 @@ public class AutonomousWrapper extends CommandGroup {
     			addSequential(new AutoDriveDistancePID(58.936, 1.0));
     			break;
     		case RobotConstants.MIDDLE_POSITION_BREACH_LEFT_SHOOT:
+    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
+    			addSequential(new AutoDriveDistancePID(-20, 1.0));
+    			addSequential(new TurnPID (-90, 0.5));
+    			addSequential(new AutoDriveDistance (35, 1.0));
+    			addSequential(new TurnPID(90,0.5));
+    			addSequential(new AutoDriveDistance(25, 1.0));
+    			//addSequential(new facegoal) 
     			break;
     		case RobotConstants.MIDDLE_POSITION_BREACH_RIGHT_NO_SHOOT:
+    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
+    			addSequential(new AutoDriveDistancePID(-20, 1.0));
+    			addSequential(new TurnPID (90, 0.5));
+    			addSequential(new AutoDriveDistance (35, 1.0));
+    			addSequential(new TurnPID(-90,0.5));
+    			addSequential(new AutoDriveDistance(25, 1.0));
     			break;
     		case RobotConstants.MIDDLE_POSITION_NO_BREACH_SHOOT:
+    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
+    			addSequential(new AutoDriveDistancePID(-20, 1.0));
+    			addSequential(new TurnPID (-90, 0.5));
+    			addSequential(new AutoDriveDistance (35, 1.0));
     			break;
     	
     	}
