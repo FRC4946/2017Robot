77937edda81391efed924c6406e45e584701af7b
Australopithecus Man Person <ZHENGSTA7@gmail.com>
2017-01-28 12:08:14 -0500

    Changed Constants.

--- src/org/usfirst/frc/team4946/robot/RobotConstants.java
+++ src/org/usfirst/frc/team4946/robot/RobotConstants.java
@@ -3,28 +3,20 @@ package org.usfirst.frc.team4946.robot;
 public class RobotConstants {
 	// For Constants that don't have to do with port numbers
 	public static final int ENCODER_PPR = 128;
-	public static final double WHEEL_DIA = 4;//3.97; // rip not even 4"
+	public static final double WHEEL_DIA = 4;// 3.97; // rip not even 4"
 	public static final double GEARBOX_REDUCTION = 24.0 / 50.0;
-	public static final double ENCODER_DISTANCE_PER_PULSE = WHEEL_DIA * Math.PI
-			/ ENCODER_PPR * GEARBOX_REDUCTION;
-	public static final double AIRSHIP_SIDELENGTH = ((Math.sin(30) * 101) / Math
-			.sin(120)); // 58.31237719 inches
+	public static final double ENCODER_DISTANCE_PER_PULSE = WHEEL_DIA * Math.PI / ENCODER_PPR * GEARBOX_REDUCTION;
+	public static final double AIRSHIP_SIDELENGTH = ((Math.sin(30) * 101) / Math.sin(120)); // 58.31237719
+																							// inches
 	public static final double AUTO_MAX_SPEED = 0.6;
 
 	public static final class Auto {
-		//Blue
-		public static final int BLUE_LEFT_POSITION_SHOOT = 1;
-		public static final int BLUE_LEFT_POSITION_NO_SHOOT = 2;
-		public static final int BLUE_RIGHT_POSITION_NO_SHOOT = 3;
-		public static final int BLUE_MIDDLE_POSITION_BREACH_LEFT_SHOOT = 4;
-		public static final int BLUE_MIDDLE_POSITION_BREACH_RIGHT_NO_SHOOT = 5;
-		public static final int BLUE_MIDDLE_POSITION_NO_BREACH_SHOOT = 6;
-		//Red
-		public static final int RED_RIGHT_POSITION_SHOOT = 7;
-		public static final int RED_RIGHT_POSITION_NO_SHOOT = 8;
-		public static final int RED_LEFT_POSITION_NO_SHOOT = 9;
-		public static final int RED_MIDDLE_POSITION_BREACH_RIGHT_SHOOT = 10;
-		public static final int RED_MIDDLE_POSITION_BREACH_LEFT_NO_SHOOT = 11;
-		public static final int RED_MIDDLE_POSITION_NO_BREACH_SHOOT = 12;
+		// Blue
+		public static final int LEFT_POSITION = 1;
+		public static final int RIGHT_POSITION = 2;
+		public static final int BREACH_NO_SHOOT = 3;
+		public static final int MIDDLE_POSITION_BREACH_SHOOT = 4;
+		public static final int MIDDLE_POSITION_BREACH_NO_SHOOT = 5;
+		public static final int MIDDLE_POSITION_NO_BREACH_SHOOT = 6;
 	}
 }
--- src/org/usfirst/frc/team4946/robot/commands/autonomous/AutonomousWrapper.java
+++ src/org/usfirst/frc/team4946/robot/commands/autonomous/AutonomousWrapper.java
@@ -7,127 +7,126 @@ import org.usfirst.frc.team4946.robot.commands.driveTrain.TurnPID;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-
 /**
  *
  */
 public class AutonomousWrapper extends CommandGroup {
 
-    public AutonomousWrapper(int mode, boolean isRed) {
-    	
-    	if(isRed) {
-    		switch (mode){
-    		case RobotConstants.Auto.RED_RIGHT_POSITION_SHOOT: 
-    			addSequential(new AutoDriveDistancePID (81.18,1.0));
-    			addSequential(new TurnPID (-60.0));
-    			addSequential(new AutoDriveDistancePID (32.026, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(96, -1.0));
-    			addSequential(new TurnPID(135.0));
-    			//addSequential(new shoot)
-    			break;
-    		case RobotConstants.Auto.RED_RIGHT_POSITION_NO_SHOOT: 
-    			addSequential(new AutoDriveDistancePID(81.18, 1.0));
-    			addSequential(new TurnPID(-60.0));
-    			addSequential(new AutoDriveDistancePID (32.026, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistance(30, -1.0));
-    			break;
-    		case RobotConstants.Auto.RED_LEFT_POSITION_NO_SHOOT:
-    			//Drives 
-    			addSequential(new AutoDriveDistancePID(67.662, 1.0));
-    			addSequential(new TurnPID(60.0));
-    			addSequential(new AutoDriveDistancePID(58.936, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(30, -1.0));
-    			break;
-    		case RobotConstants.Auto.RED_MIDDLE_POSITION_BREACH_RIGHT_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (90));
-    			addSequential(new AutoDriveDistance (35, 1.0));
-    			addSequential(new TurnPID(-90));
-    			addSequential(new AutoDriveDistance(35, 1.0));
-    			addSequential(new TurnPID(110));
-    			//addSequential(new facegoal) 
-    			break;
-    		case RobotConstants.Auto.RED_MIDDLE_POSITION_BREACH_LEFT_NO_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//Drop Gear
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (90));
-    			addSequential(new AutoDriveDistance (35, 1.0));
-    			addSequential(new TurnPID(-90));
-    			addSequential(new AutoDriveDistance(35, 1.0));
-    			break;
-    		case RobotConstants.Auto.RED_MIDDLE_POSITION_NO_BREACH_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//drop gear 
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (-90));
-    			addSequential(new AutoDriveDistancePID(50, 1.0));
-    			addSequential(new TurnPID(-20));
-    			//shoot
-    			break;
-    		}
-    	} else {
-    		switch (mode){
-    		case RobotConstants.Auto.BLUE_LEFT_POSITION_SHOOT: 
-    			addSequential(new AutoDriveDistancePID (81.18,1.0));
-    			addSequential(new TurnPID (60.0));
-    			addSequential(new AutoDriveDistancePID (32.026, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(96, -1.0));
-    			addSequential(new TurnPID(135));
-    			//addSequential(new shoot)
-    			break;
-    		case RobotConstants.Auto.BLUE_LEFT_POSITION_NO_SHOOT: 
-    			addSequential(new AutoDriveDistancePID(81.18, 1.0));
-    			addSequential(new TurnPID(60.0));
-    			addSequential(new AutoDriveDistancePID (32.026, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistance(30, -1.0));
-    			break;
-    		case RobotConstants.Auto.BLUE_RIGHT_POSITION_NO_SHOOT:
-    			//Drives 
-    			addSequential(new AutoDriveDistancePID(67.662, 1.0));
-    			addSequential(new TurnPID(-60.0));
-    			addSequential(new AutoDriveDistancePID(58.936, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(30, -1.0));
-    			break;
-    		case RobotConstants.Auto.BLUE_MIDDLE_POSITION_BREACH_LEFT_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//drop gear
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (-90));
-    			addSequential(new AutoDriveDistance (35, 1.0));
-    			addSequential(new TurnPID(90));
-    			addSequential(new AutoDriveDistance(35, 1.0));
-    			addSequential(new TurnPID(-110));
-    			//addSequential(new facegoal) 
-    			break;
-    		case RobotConstants.Auto.BLUE_MIDDLE_POSITION_BREACH_RIGHT_NO_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//Drop Gear
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (90));
-    			addSequential(new AutoDriveDistance (35, 1.0));
-    			addSequential(new TurnPID(-90));
-    			addSequential(new AutoDriveDistance(35, 1.0));
-    			break;
-    		case RobotConstants.Auto.BLUE_MIDDLE_POSITION_NO_BREACH_SHOOT:
-    			addSequential(new AutoDriveDistancePID(70.6, 1.0));
-    			//drop gear 
-    			addSequential(new AutoDriveDistancePID(20, -1.0));
-    			addSequential(new TurnPID (-90));
-    			addSequential(new AutoDriveDistancePID(50, 1.0));
-    			addSequential(new TurnPID(-20));
-    			//shoot
-    			break;
-    		}
-    	}
-    	
-    }
+	public AutonomousWrapper(int mode, boolean isRed) {
+
+		if (isRed) {
+			switch (mode) {
+			case RobotConstants.Auto.RIGHT_POSITION:
+				addSequential(new AutoDriveDistancePID(81.18, 1.0));
+				addSequential(new TurnPID(-60.0));
+				addSequential(new AutoDriveDistancePID(32.026, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(96, -1.0));
+				addSequential(new TurnPID(135.0));
+				// addSequential(new shoot)
+				break;
+			case RobotConstants.Auto.BREACH_NO_SHOOT:
+				addSequential(new AutoDriveDistancePID(81.18, 1.0));
+				addSequential(new TurnPID(-60.0));
+				addSequential(new AutoDriveDistancePID(32.026, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistance(30, -1.0));
+				break;
+			case RobotConstants.Auto.LEFT_POSITION_NO_SHOOT:
+				// Drives
+				addSequential(new AutoDriveDistancePID(67.662, 1.0));
+				addSequential(new TurnPID(60.0));
+				addSequential(new AutoDriveDistancePID(58.936, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(30, -1.0));
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_BREACH_RIGHT_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(-90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(110));
+				// addSequential(new facegoal)
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_BREACH_LEFT_NO_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// Drop Gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(-90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_NO_BREACH_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(90));
+				addSequential(new AutoDriveDistancePID(50, 1.0));
+				addSequential(new TurnPID(20));
+				// shoot
+				break;
+			}
+		} else {
+			switch (mode) {
+			case RobotConstants.Auto.LEFT_POSITION_SHOOT:
+				addSequential(new AutoDriveDistancePID(81.18, 1.0));
+				addSequential(new TurnPID(60.0));
+				addSequential(new AutoDriveDistancePID(32.026, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(96, -1.0));
+				addSequential(new TurnPID(135));
+				// addSequential(new shoot)
+				break;
+			case RobotConstants.Auto.LEFT_POSITION_NO_SHOOT:
+				addSequential(new AutoDriveDistancePID(81.18, 1.0));
+				addSequential(new TurnPID(60.0));
+				addSequential(new AutoDriveDistancePID(32.026, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistance(30, -1.0));
+				break;
+			case RobotConstants.Auto.RIGHT_POSITION_NO_SHOOT:
+				// Drives
+				addSequential(new AutoDriveDistancePID(67.662, 1.0));
+				addSequential(new TurnPID(-60.0));
+				addSequential(new AutoDriveDistancePID(58.936, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(30, -1.0));
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_BREACH_LEFT_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(-90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(-110));
+				// addSequential(new facegoal)
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_BREACH_RIGHT_NO_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// Drop Gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				addSequential(new TurnPID(-90));
+				addSequential(new AutoDriveDistance(35, 1.0));
+				break;
+			case RobotConstants.Auto.MIDDLE_POSITION_NO_BREACH_SHOOT:
+				addSequential(new AutoDriveDistancePID(70.6, 1.0));
+				// drop gear
+				addSequential(new AutoDriveDistancePID(20, -1.0));
+				addSequential(new TurnPID(-90));
+				addSequential(new AutoDriveDistancePID(50, 1.0));
+				addSequential(new TurnPID(-20));
+				// shoot
+				break;
+			}
+		}
+
+	}
 }
