f3a092edd2f321369d4ef72416adc09281ac8d0a
betha <betha@10.31.201.8>
2017-02-09 16:46:11 -0500

    # WARNING: head commit changed in the meantime
    fli

---
+++ src/org/usfirst/frc/team4946/robot/commands/SpinShooter.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team4946.robot.commands;
+
+import org.usfirst.frc.team4946.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class SpinShooter extends Command {
+
+    public SpinShooter() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.shooterSubsystem);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.shooterSubsystem.setRPM(Robot.shooterSubsystem.getSetRPM());
+    	SmartDashboard.getNumber("RPM: ", Robot.shooterSubsystem.getSetRPM());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.shooterSubsystem.setSpeed(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
